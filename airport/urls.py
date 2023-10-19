94912230e0dd12e82e85b077d6319067da1a301d
Eduard Habryd <eduard.boxing2001@gmail.com>
2023-10-19 21:50:33 +0300

    add routes for all the models

--- airport/urls.py
+++ airport/urls.py
@@ -1,8 +1,28 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 
-urlpatterns = [
-    ...
-]
+from airport.views import (
+	AirportViewSet,
+	RouteViewSet,
+	AirplaneTypeViewSet,
+	AirplaneViewSet,
+	CrewViewSet,
+	FlightViewSet,
+	OrderViewSet,
+	TicketViewSet
+)
+
+router = routers.DefaultRouter()
+router.register("airports", AirportViewSet)
+router.register("routes", RouteViewSet)
+router.register("airplane-types", AirplaneTypeViewSet)
+router.register("airplanes", AirplaneViewSet)
+router.register("crew", CrewViewSet)
+router.register("flights", FlightViewSet)
+router.register("orders", OrderViewSet)
+router.register("tickets", TicketViewSet)
+
+urlpatterns = [path("api/", include(router.urls))]
 
 app_name = "airport"
