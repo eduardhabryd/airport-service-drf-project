4d13806bc12d44dbd60d6c7db64183d5f6fa121d
Eduard Habryd <eduard.boxing2001@gmail.com>
2023-10-19 19:00:20 +0300

    add groups and user_permissions to User model

--- user/models.py
+++ user/models.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import (
     AbstractUser,
-    BaseUserManager,
+    BaseUserManager, Group, Permission,
 )
 from django.db import models
 from django.utils.translation import gettext as _
@@ -48,3 +48,6 @@ class User(AbstractUser):
     REQUIRED_FIELDS = []
 
     objects = UserManager()
+    
+    groups = models.ManyToManyField(Group, related_name="custom_user_set")
+    user_permissions = models.ManyToManyField(Permission, related_name="custom_user_set")
