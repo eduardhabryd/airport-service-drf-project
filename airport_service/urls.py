18123410a3d83e930c8b0641d33433e9926882bf
Eduard Habryd <eduard.boxing2001@gmail.com>
2023-10-19 18:27:27 +0300

    add path for api/user/, for drf_spectacular, and include path for airport

--- airport_service/urls.py
+++ airport_service/urls.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularSwaggerView, SpectacularRedocView, SpectacularAPIView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("airport.urls", namespace="airport"))
+    path("api/user/", include("user.urls", namespace="user")),
+    path("", include("airport.urls", namespace="airport")),
+    path("__debug__/", include("debug_toolbar.urls")),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/doc/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "api/doc/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
 ]
