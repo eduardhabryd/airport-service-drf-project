363c8b40687f86a65765314440309ecd80d8951b
Eduard Habryd <eduard.boxing2001@gmail.com>
2023-10-20 15:23:08 +0300

    add str methods to Airport, Route, AirplaneType, Crew, Flight; add unique_together to Ticket

--- airport/models.py
+++ airport/models.py
@@ -5,16 +5,24 @@ from django.db import models
 class Airport(models.Model):
 	name = models.CharField(max_length=100)
 	closest_big_city = models.CharField(max_length=100)
+	
+	def __str__(self):
+		return f"AIRPORT: {self.name}. CITY: {self.closest_big_city}"
 
 
 class Route(models.Model):
 	source = models.ForeignKey(Airport, related_name="source_routes", on_delete=models.CASCADE)
 	destination = models.ForeignKey(Airport, related_name="destination_routes", on_delete=models.CASCADE)
 	distance = models.IntegerField()
-
+	
+	def __str__(self):
+		return f"ROUTE: {self.source.closest_big_city} - {self.destination.closest_big_city}. DISTANCE: {self.distance}"
 
 class AirplaneType(models.Model):
 	name = models.CharField(max_length=100)
+	
+	def __str__(self):
+		return f"TYPE: {self.name}"
 
 
 class Airplane(models.Model):
@@ -22,11 +30,21 @@ class Airplane(models.Model):
 	rows = models.IntegerField()
 	seats_in_row = models.IntegerField()
 	airplane_type = models.ForeignKey(AirplaneType, related_name="airplanes", on_delete=models.CASCADE)
-
+	
+	@property
+	def capacity(self):
+		return self.rows * self.seats_in_row
+	
+	def __str__(self):
+		return f"AIRPLANE: {self.name}"
+	
 
 class Crew(models.Model):
 	first_name = models.CharField(max_length=100)
 	last_name = models.CharField(max_length=100)
+	
+	def __str__(self):
+		return f"CREW MEMBER: {self.first_name} {self.last_name}"
 
 
 class Flight(models.Model):
@@ -35,10 +53,13 @@ class Flight(models.Model):
 	departure_time = models.DateTimeField()
 	arrival_time = models.DateTimeField()
 	crew = models.ManyToManyField(Crew, related_name="flights")
+	
+	def __str__(self):
+		return f"FLIGHT: {self.departure_time} - {self.arrival_time}. ROUTE: {self.route}"
 
 
 class Order(models.Model):
-	created_at = models.DateTimeField()
+	created_at = models.DateTimeField(auto_now_add=True)
 	user = models.ForeignKey(get_user_model(), related_name="orders", on_delete=models.CASCADE)
 
 
@@ -47,3 +68,7 @@ class Ticket(models.Model):
 	seat = models.IntegerField()
 	flight = models.ForeignKey(Flight, related_name="tickets", on_delete=models.CASCADE)
 	order = models.ForeignKey(Order, related_name="tickets", on_delete=models.CASCADE)
+	
+	class Meta:
+		unique_together = ("flight", "row", "seat")
+		ordering = ["row", "seat"]
